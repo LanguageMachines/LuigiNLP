8848f1f43b7a352cd8277edb2756bbac600fb482
Maarten van Gompel <proycon@anaproy.nl>
2016-05-20 14:55:17 +0200

    Frog workflow component now uses autosetup

--- piccl/modules/frog.py
+++ piccl/modules/frog.py
@@ -8,28 +8,6 @@ from piccl.inputs import FoLiAInput, PlainTextInput
 
 log = logging.getLogger('mainlog')
 
-class Frog(WorkflowComponent):
-    """A workflow component for Frog"""
-
-    skip = Parameter(default="") #A parameter for the workflow, will be passed on to the tasks
-
-    def accepts(self):
-        """Returns a tuple of all the initial inputs and other workflows this component accepts as input (a disjunction)"""
-        return (FoLiAInput, PlainTextInput, InputWorkflow(self, ConvertToFoLiA) )
-
-    def setup(self, workflow):
-        """The actual workflow specification"""
-        #setup the input, invokes dependency workflows when needed
-        input_type, input_slot = self.setup_input(workflow)
-        if input_type == 'txt':
-            frog = workflow.new_task('frog', Frog_txt2folia,skip=self.skip ) #add a task, passing parameters
-            frog.in_txt = input_slot #set the input slot of the task to that of the workflow component
-        elif input_type == 'folia':
-            frog = workflow.new_task('frog', Frog_folia2folia,skip=self.skip ) #add a task, passing parameters
-            frog.in_folia = input_slot #set the input slot of the task to that of the workflow component
-        return 'folia', frog #return the type of output  and the last task of the workflow (mandatory!), the last task must have an out_* slot named as specified here.
-
-Frog.inherit_parameters(ConvertToFoLiA)
 
 class Frog_txt2folia(Task):
     """A task for Frog: Takes plaintext input and produces FoLiA output"""
@@ -75,3 +53,36 @@ class Frog_folia2folia(Task):
             X=self.out_folia().path,
             skip=self.skip if self.skip else None,
         )
+
+#################################################################################################################
+# Workflow Components
+#################################################################################################################
+
+class Frog(WorkflowComponent):
+    """A workflow component for Frog"""
+
+    skip = Parameter(default="") #A parameter for the workflow, will be passed on to the tasks
+
+    autosetup = (Frog_txt2folia, Frog_folia2folia)
+
+    def accepts(self):
+        """Returns a tuple of all the initial inputs and other workflows this component accepts as input (a disjunction)"""
+        return (FoLiAInput, PlainTextInput, InputWorkflow(self, ConvertToFoLiA) )
+
+
+    #Commented out the below setup() method because autosetup generates this code automatically now
+    #Leaving it here as a reference, as autosetup won't suffice for more complex workflows or when slot/parameter mappings are needed
+
+    #def setup(self, workflow):   
+    #    """The actual workflow specification"""
+    #    #setup the input, invokes dependency workflows when needed
+    #    input_type, input_slot = self.setup_input(workflow)
+    #    if input_type == 'txt':
+    #        frog = workflow.new_task('frog', Frog_txt2folia,skip=self.skip ) #add a task, passing parameters
+    #        frog.in_txt = input_slot #set the input slot of the task to that of the workflow component
+    #    elif input_type == 'folia':
+    #        frog = workflow.new_task('frog', Frog_folia2folia,skip=self.skip ) #add a task, passing parameters
+    #        frog.in_folia = input_slot #set the input slot of the task to that of the workflow component
+    #    return 'folia', frog #return the type of output  and the last task of the workflow (mandatory!), the last task must have an out_* slot named as specified here.
+
+Frog.inherit_parameters(ConvertToFoLiA) #important, inherit parameters of all our possible dependencies
