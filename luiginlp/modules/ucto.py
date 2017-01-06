da6a51756886d847e10f8ba93aed0f071b495d48
Maarten van Gompel <proycon@anaproy.nl>
2017-01-06 14:33:44 +0100

    added Ucto_tok2folia class that uses ucto's --passthru

--- luiginlp/modules/ucto.py
+++ luiginlp/modules/ucto.py
@@ -60,6 +60,25 @@ class Ucto_folia2folia(Task):
                 F=True, #folia input
                 X=True) #folia output
 
+class Ucto_tok2folia(Task):
+    executable = 'ucto' #external executable (None if n/a)
+
+    #Parameters for this module (all mandatory!)
+    language = Parameter()
+    tok_input_sentenceperline = BoolParameter(default=False)
+
+    in_tok = InputSlot() #will be linked to an out_* slot of another module in the workflow specification
+
+    def out_folia(self):
+        return self.outputfrominput(inputformat='tok',stripextension='.tok', addextension='.folia.xml')
+
+    def run(self):
+        self.ex(self.in_txt().path, self.out_folia().path,
+                L=self.language,
+                m=self.tok_input_sentenceperline,
+                passthru=True,
+                X=True)
+
 #################################################################################################################
 # Workflow Components
 #################################################################################################################
@@ -75,13 +94,14 @@ class Ucto(StandardWorkflowComponent):
     tok_output_sentenceperline = BoolParameter(default=False)
 
     def autosetup(self):
-        return (Ucto_txt2folia, Ucto_folia2folia)
+        return (Ucto_txt2folia, Ucto_folia2folia, Ucto_tok2folia)
 
     def accepts(self):
         """Returns a tuple of all the initial inputs and other workflows this component accepts as input (a disjunction, only one will be selected)"""
         return (
             InputFormat(self, format_id='folia', extension='folia.xml'),
             InputFormat(self, format_id='txt', extension='txt'),
+            InputFormat(self, format_id='tok', extension='tok'),
             InputComponent(self, ConvertToFoLiA))
 
 
