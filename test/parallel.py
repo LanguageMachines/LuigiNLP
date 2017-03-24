43ba6ad7396c8ca00e661cbab160420ed30ec8a8
Maarten van Gompel <proycon@anaproy.nl>
2017-03-24 17:42:32 +0100

    fixed

--- test/parallel.py
+++ test/parallel.py
@@ -1,4 +1,4 @@
 import luiginlp
 from luiginlp.modules.frog import Frog
-from luiginlp.engine import Parallel, ComponentParameters
-luiginlp.run(Parallel(component='Frog',inputfiles="test.rst,test2.rst",component_parameters=ComponentParameters(skip='p')))
+from luiginlp.engine import Parallel, PassParameters
+luiginlp.run(Parallel(component='Frog',inputfiles="test.rst,test2.rst",passparameters=PassParameters(skip='p')))
